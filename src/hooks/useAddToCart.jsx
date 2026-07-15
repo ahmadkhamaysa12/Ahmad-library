bb7cc79f93f9702def363c80182f1951ed4a4230
ahmadkhamaysa12 <ahmadkhamaysa2023@gmail.com>
2026-07-15 18:30:27 +0300

    all add for now

--- src/hooks/useAddToCart.jsx
+++ src/hooks/useAddToCart.jsx
@@ -3,17 +3,21 @@ import authAxiosInstance from '../api/authAxiosInstance';
 
 export default function useAddToCart() {
   const queryClient = useQueryClient();
-  const mutation = useMutation({
+
+  return useMutation({
     mutationFn: async ({ ProductId, Count }) => {
-      return await authAxiosInstance.post('/Carts', {
-        ProductId: ProductId,
-        Count: Count,
+      const response = await authAxiosInstance.post('/Carts', {
+        ProductId,
+        Count,
+      });
+
+      return response.data;
+    },
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['cart'],
       });
     },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['cart'] });
-    },
   });
-
-  return mutation;
 }
