34558d4adffe6e8235bb1a24a22756a4dfa2ed17
ahmadkhamaysa12 <ahmadkhamaysa2023@gmail.com>
2026-07-23 18:30:50 +0300

    book is done

--- src/hooks/useBook.jsx
+++ src/hooks/useBook.jsx
@@ -1,15 +1,17 @@
 import { useQuery } from '@tanstack/react-query';
+import { useTranslation } from 'react-i18next';
 import axiosInstance from '@/api/axiosInstance';
 
-
 export default function useBook(id) {
+  const { i18n } = useTranslation();
+
   const getBook = async () => {
     const response = await axiosInstance.get(`/Products/${id}`);
     return response.data.response;
   };
 
   return useQuery({
-    queryKey: ['book', id],
+    queryKey: ['book', i18n.language, id],
     queryFn: getBook,
     enabled: !!id,
     staleTime: 1000 * 60 * 2,
