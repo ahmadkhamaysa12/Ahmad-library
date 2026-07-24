3dd1bc45598880540fde3c1afc4c3e73992c0e31
ahmadkhamaysa12 <ahmadkhamaysa2023@gmail.com>
2026-07-24 14:42:44 +0300

    update pass hook

---
+++ src/hooks/useUpdatePassword.jsx
@@ -0,0 +1,15 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import authinstance from '../api/authAxiosInstance';
+
+export default function useUpdatePassword() {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (data) => authinstance.patch(`/Profile/change-password`, data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['profile'] });
+    },
+    onError: (error) => {
+      console.error('Failed to update password', error);
+    },
+  });
+}
