abe43671ae571a9f9e423c0d141499d00171b2ea
ahmadkhamaysa12 <ahmadkhamaysa2023@gmail.com>
2026-08-03 20:45:16 +0300

    final app

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,11 @@ export default function App() {
   const { i18n } = useTranslation();
 
   useEffect(() => {
-    document.documentElement.lang = i18n.language;
-    document.documentElement.dir = i18n.language === 'ar' ? 'rtl' : 'ltr';
-  }, [i18n.language]);
+    const { documentElement } = document;
+
+    documentElement.lang = i18n.language;
+    documentElement.dir = i18n.dir(i18n.language);
+  }, [i18n]);
 
   return <RouterProvider router={router} />;
 }
